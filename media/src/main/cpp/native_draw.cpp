6d5b7480ab17af16a74fbf4532b46867d4b036ed
hyUrbanManager <936947038@qq.com>
2018-03-24 20:23:20 +0800

    1、native层绘制单色调。

--- media/src/main/cpp/native_draw.cpp
+++ media/src/main/cpp/native_draw.cpp
@@ -2,17 +2,58 @@
 // Created by Administrator on 2018/3/24.
 //
 #include <jni.h>
+#include <unistd.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <android/log.h>
+
+#define log(...) \
+        __android_log_print(ANDROID_LOG_INFO, "@jni", __VA_ARGS__)
+
+extern "C" {
 
 JNIEXPORT void JNICALL
 Java_com_hy_media_NativeDrawActivity_native_1draw1(JNIEnv* env, jobject instance, jobject surface) {
+    ANativeWindow* window = ANativeWindow_fromSurface(env, surface);
+    ANativeWindow_acquire(window);
 
-// TODO
+    int width = ANativeWindow_getWidth(window);
+    int height = ANativeWindow_getHeight(window);
+    int format = ANativeWindow_getFormat(window);
+    log("width: %d, height: %d, format: %d", width, height, format);
 
+    ANativeWindow_Buffer buffer;
+
+    // 设置rgb 565
+    int set_ret = ANativeWindow_setBuffersGeometry(window, width, height, format);
+    int lock_ret = ANativeWindow_lock(window, &buffer, NULL);
+
+    memset(buffer.bits, 0x55, (size_t) (buffer.stride * height * 2));
+
+    int unlock_ret = ANativeWindow_unlockAndPost(window);
+    ANativeWindow_release(window);
+
+    log("set ret: %d, lock ret: %d, unlock_ret: %d", set_ret, lock_ret, unlock_ret);
 }
 
 JNIEXPORT void JNICALL
 Java_com_hy_media_NativeDrawActivity_native_1draw2(JNIEnv* env, jobject instance, jobject surface) {
+    ANativeWindow* window = ANativeWindow_fromSurface(env, surface);
 
-// TODO
+    int width = 640, height = 360;
+    int format = ANativeWindow_getFormat(window);
+    log("width: %d, height: %d, format: %d", width, height, format);
+
+    ANativeWindow_Buffer buffer;
+
+    // 设置rgb 565
+    int set_ret = ANativeWindow_setBuffersGeometry(window, width, height, format);
+    int lock_ret = ANativeWindow_lock(window, &buffer, NULL);
+
+    memset(buffer.bits, 0x22, (size_t) (width * height * 2));
+
+    int unlock_ret = ANativeWindow_unlockAndPost(window);
+    log("set ret: %d, lock ret: %d, unlock_ret: %d", set_ret, lock_ret, unlock_ret);
+}
 
 }
--- media/src/main/java/com/hy/media/NativeDrawActivity.java
+++ media/src/main/java/com/hy/media/NativeDrawActivity.java
@@ -1,16 +1,30 @@
 package com.hy.media;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Surface;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NativeDrawActivity extends AppCompatActivity {
+
+    static {
+        try {
+            Class.forName("com.hy.media.player.FFPlayer");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     @BindView(R.id.surfaceView)
     SurfaceView surfaceView;
@@ -20,6 +34,15 @@ public class NativeDrawActivity extends AppCompatActivity {
     Button button2;
     @BindView(R.id.line1)
     LinearLayout line1;
+    @BindView(R.id.button3)
+    Button button3;
+    @BindView(R.id.button4)
+    Button button4;
+
+    private Random random = new Random();
+    private boolean isStart = false;
+
+    private Handler mMainHandler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +50,81 @@ public class NativeDrawActivity extends AppCompatActivity {
         setContentView(R.layout.activity_native_draw);
         ButterKnife.bind(this);
 
+        button1.setOnClickListener(v -> {
+            native_draw1(surfaceView.getHolder().getSurface());
+        });
+
+        button2.setOnClickListener(v -> {
+            native_draw2(surfaceView.getHolder().getSurface());
+        });
+
+        button3.setOnClickListener(v -> {
+            SurfaceHolder holder = surfaceView.getHolder();
+            Surface surface = holder.getSurface();
+
+            // 获取画布。
+            Canvas canvas = holder.lockCanvas();
+
+            canvas.drawARGB(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+
+            holder.unlockCanvasAndPost(canvas);
+        });
+
+        button4.setOnClickListener(v -> {
+            if (isStart) {
+                return;
+            }
+            isStart = true;
+
+            new Thread(() -> {
+                int r = 255, g = 0, b = 0;
+                int dir = 0;
+                while (isStart) {
+                    if (dir == 0) {
+                        r--;
+                    } else {
+                        r++;
+                    }
+                    if (r >= 255) {
+                        dir = 0;
+                        r = 255;
+                    }
+                    if (r <= 0) {
+                        dir = 1;
+                        r = 0;
+                    }
+                    g = 255 - r;
+
+//                    b += 2;
+//                    if (b >= 255) {
+//                        b = 0;
+//                    }
+
+                    setColor(r, g, b);
+
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isStart = false;
+    }
+
+    private void setColor(int r, int g, int b) {
+        SurfaceHolder holder = surfaceView.getHolder();
+        Canvas canvas = holder.lockCanvas();
+        if (canvas != null) {
+            canvas.drawARGB(255, r, g, b);
+            holder.unlockCanvasAndPost(canvas);
+        }
     }
 
     private native void native_draw1(Surface surface);
