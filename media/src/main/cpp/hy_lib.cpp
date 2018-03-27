b8f90e2b8a6f7b05de1643881cca61a058f48f85
hyUrbanManager <936947038@qq.com>
2018-03-27 11:31:31 +0800

    1、增加数字转字符串。

--- media/src/main/cpp/hy_lib.cpp
+++ media/src/main/cpp/hy_lib.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "hy_lib.h"
+#include <unistd.h>
+
+static const char* internal_ntos(long l);
+
+/*
+ * 数字转字符串。
+ */
+const char* itos(int i) {
+    return internal_ntos(i);
+}
+
+/*
+ * 内部转换数字。
+ */
+static const char* internal_ntos(long l) {
+    char* str = (char*) calloc(1, 15);
+    int di = 0;
+    if (l < 0) {
+        str[0] = '-';
+        di = 1;
+    }
+
+    char tcs[15] = {0};
+    int si = 0;
+    long t = l;
+    while (t > 0) {
+        tcs[si++] = (char) (t % 10 + '0');
+        t /= 10;
+    }
+
+    while (si >= 0) {
+        str[di++] = tcs[--si];
+    }
+
+    return str;
+}
--- media/src/main/cpp/hy_lib.h
+++ media/src/main/cpp/hy_lib.h
@@ -5,4 +5,9 @@
 #ifndef HACKATHON_HY_LIB_H
 #define HACKATHON_HY_LIB_H
 
+/*
+ * 数字转字符串。
+ */
+const char* itos(int i);
+
 #endif //HACKATHON_HY_LIB_H
