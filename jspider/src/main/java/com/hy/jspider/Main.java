9073817e99b97b3fe5712f884d9b8ba3f5180f76
hyUrbanManager <936947038@qq.com>
2018-05-10 22:38:25 +0800

    加入log4j配置。 成功抓取到自己的网页。

--- jspider/src/main/java/com/hy/jspider/Main.java
+++ jspider/src/main/java/com/hy/jspider/Main.java
@@ -1,4 +1,67 @@
 package com.hy.jspider;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+
 public class Main {
+
+    @Test
+    public void run() {
+//        System.setProperty("javax.net.debug", "all");
+        main(new String[]{"123"});
+    }
+
+    public static void main(String[] args) {
+        pwd();
+
+        initLog4jConfig();
+
+        Spider.create(new MyWebProcessor())
+                .addUrl("http://139.199.170.98")
+                .thread(4)
+                .addPipeline(new JsonFilePipeline("G:\\webmagic\\myweb"))
+                .run();
+    }
+
+    private static void initLog4jConfig() {
+        Properties props;
+        FileInputStream fis = null;
+        try {
+            // 从配置文件dbinfo.properties中读取配置信息
+            props = new Properties();
+            fis = new FileInputStream("jspider/log4j.properties");
+            props.load(fis);
+            PropertyConfigurator.configure(props);//装入log4j配置信息
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void pwd() {
+        File directory = new File("");//参数为空
+        String courseFile = null;
+        try {
+            courseFile = directory.getCanonicalPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(courseFile);
+    }
+
 }
---
+++ jspider/src/main/java/com/hy/jspider/MyWebProcessor.java
@@ -0,0 +1,26 @@
+package com.hy.jspider;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by Administrator on 2018/5/10.
+ *
+ * @author hy 2018/5/10
+ */
+public class MyWebProcessor implements PageProcessor{
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    public void process(Page page) {
+        page.putField("h1", page.getHtml().xpath("//h1[href]").toString());
+        page.putField("links", page.getHtml().links());
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+}
