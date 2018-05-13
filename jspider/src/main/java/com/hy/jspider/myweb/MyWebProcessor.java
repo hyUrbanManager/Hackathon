3265cea8b0d710ac0d67c8a22ee153493ad3154f
hyUrbanManager <936947038@qq.com>
2018-05-14 00:09:18 +0800

    爬取我的网页下载链接。

--- jspider/src/main/java/com/hy/jspider/myweb/MyWebProcessor.java
+++ jspider/src/main/java/com/hy/jspider/myweb/MyWebProcessor.java
@@ -1,11 +1,14 @@
 package com.hy.jspider.myweb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
- * Created by Administrator on 2018/5/10.
+ * 爬取所有下载链接。
  *
  * @author hy 2018/5/10
  */
@@ -15,7 +18,15 @@ public class MyWebProcessor implements PageProcessor{
 
     @Override
     public void process(Page page) {
-        page.putField("h1", page.getHtml().xpath("//h1/a/@href").get());
+        List<String> list = page.getHtml().xpath("//a/@href").all();
+        List<String> list2 = new ArrayList<>();
+        for (String link : list) {
+            if (link.contains(".")) {
+                list2.add(link);
+            }
+        }
+
+        page.putField("links", list2);
     }
 
     @Override
