9418a2e9c7ad30bc5dc306e496a41c1e0f4f5e73
hyUrbanManager <936947038@qq.com>
2018-05-13 11:43:30 +0800

    windows linux下使用不同的log4j配置。 每次运行时输出version，确保linux上运行的是新版本。

--- jspider/src/main/java/com/hy/jspider/Main.java
+++ jspider/src/main/java/com/hy/jspider/Main.java
@@ -13,6 +13,8 @@ import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 
 public class Main {
 
+    public static final String version = "0.1";
+
     @Test
     public void run() {
 //        System.setProperty("javax.net.debug", "all");
@@ -20,9 +22,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        System.out.println("version: " + version);
+
         pwd();
 
-        initLog4jConfig();
+        String os = System.getProperty("os.name").toLowerCase();
+        System.out.println("operating system: " + os);
+
+        if (os.contains("windows")) {
+            initLog4jConfig(true);
+        } else {
+            initLog4jConfig(false);
+        }
 
         Spider.create(new MyWebProcessor())
                 .addUrl("http://139.199.170.98")
@@ -31,13 +42,17 @@ public class Main {
                 .run();
     }
 
-    private static void initLog4jConfig() {
+    private static void initLog4jConfig(boolean isWindows) {
         Properties props;
         FileInputStream fis = null;
         try {
             // 从配置文件dbinfo.properties中读取配置信息
             props = new Properties();
-            fis = new FileInputStream("jspider/log4j.properties");
+            if (isWindows) {
+                fis = new FileInputStream("jspider/log4jw.properties");
+            } else {
+                fis = new FileInputStream("/home/hy/Public/log4jl.properties");
+            }
             props.load(fis);
             PropertyConfigurator.configure(props);//装入log4j配置信息
         } catch (Exception e) {
