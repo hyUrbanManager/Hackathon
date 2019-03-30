762387aec2b6f8b77abc2722b3cb1e801a759c8a
huangye <huangye@cvte.com>
2019-03-30 10:05:36 +0800

    [feature]增加opengl工程，测试运行opengl的程序。

---
+++ opengl/src/main/java/com/hy/opengl/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.hy.opengl
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
+
